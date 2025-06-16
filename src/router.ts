95d5d2a9bc5e908fb242ff874113340b97f9c704
khemerak <pavkhemerak19@gmail.com>
2025-06-16 11:24:57 +0700

    update

---
+++ src/router.ts
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    // path: '/',
+    // element: <Root />,
+    // children: [
+    //   {
+    //     path: '/',
+    //     element: <Home />,
+    //   },
+    //   {
+    //     path: '/projects',
+    //     element: <Projects />,
+    //   },
+    //   {
+    //     path: '/contact',
+    //     element: <Contact />,
+    //   },
+    // ],
+  },
+])
